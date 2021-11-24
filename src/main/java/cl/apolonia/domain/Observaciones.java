22495d9ca525f3406753e50ec2f6e61aa4f96637
Bellann <mf.chaconp@gmail.com>
2021-11-24 11:48:03 -0300

    entidades
    creada Observaciones
    Modificadas TareasEjecutadas

---
+++ src/main/java/cl/apolonia/domain/Observaciones.java
@@ -0,0 +1,100 @@
+package cl.apolonia.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="RESP_TAREA_EJECUTADAS")
+public class Observaciones implements Serializable{
+   
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="ID", insertable = false, updatable = false)
+    private Integer id;
+    
+    @Column(name="IDTAREA")
+    private Integer idtarea;
+    
+    @Column(name="FECHA")
+    private Date fecha;
+    
+    @Column(name="RUN")
+    private String run;
+    
+    @Column(name="NOMBRE")
+    private String nombre;
+    
+    @Column(name="COMENTARIO")
+    private String comentario;
+
+    public Observaciones() {
+    }
+
+    public Observaciones(Integer id, Integer idtarea, Date fecha, String run, String nombre, String comentario) {
+        this.id = id;
+        this.idtarea = idtarea;
+        this.fecha = fecha;
+        this.run = run;
+        this.nombre = nombre;
+        this.comentario = comentario;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getIdtarea() {
+        return idtarea;
+    }
+
+    public void setIdtarea(Integer idtarea) {
+        this.idtarea = idtarea;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getRun() {
+        return run;
+    }
+
+    public void setRun(String run) {
+        this.run = run;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+    
+    
+    
+    
+}
