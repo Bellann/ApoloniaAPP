dff6a08611c9b3dc5c51a99e468f9d2ac0f67c6c
Bellann <mf.chaconp@gmail.com>
2021-11-25 16:42:29 -0300

    ejecutar proceso
    Ejecutar proceso 1 listo

--- src/main/java/cl/apolonia/web/ControladorProceso.java
+++ src/main/java/cl/apolonia/web/ControladorProceso.java
@@ -5,6 +5,7 @@ import cl.apolonia.dao.ProcesosDao;
 import cl.apolonia.dao.ProcesosTipoDao;
 import cl.apolonia.dao.TareasEjecutadasDao;
 import cl.apolonia.dao.TareasTipoDao;
+import cl.apolonia.domain.TareasEjecutadas;
 import cl.apolonia.domain.TareasTipo;
 import cl.apolonia.service.FuncionariosService;
 import cl.apolonia.service.ProcesoEjecutadosService;
@@ -12,16 +13,18 @@ import cl.apolonia.service.ProcesosSerivce;
 import cl.apolonia.service.ProcesosTipoService;
 import cl.apolonia.service.TareasEjecutadasServices;
 import cl.apolonia.service.procParticipoService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ControladorProceso {
-           
+
     @Autowired
     private FuncionariosService funcionariosService;
 
@@ -54,26 +57,26 @@ public class ControladorProceso {
 
     @Autowired
     private TareasTipoDao tareasTipoDao;
-     @GetMapping("/ejecutarproceso")
+
+    @GetMapping("/ejecutarproceso")
     public String ejecutarproceso(Model model) {
         var procesos = procesosService.listarProcesos();
         var nombreCompleto = funcionariosService.nombreCompleto();
         var rolSaludo = funcionariosService.rolSaludo();
         var listaProcesos = procesosTipoService.listarPorUnidad(funcionariosService.rutUnidad());
-        
+
         //var procesotipo = procesosTipoDao.findByNombre("Prueba Proceso");
         //model.addAttribute("procesotipo", procesotipo);
         model.addAttribute("nusuario", nombreCompleto);
         model.addAttribute("rolsaludo", rolSaludo);
         model.addAttribute("procesos", procesos);
         model.addAttribute("listaProcesos", listaProcesos);
-        
 
         return "ejecutarproceso";
     }
 
     @GetMapping(value = {"/enviarid"})
-    public String tareasporproceso(@RequestParam(value = "nombre") String urlParam, Model model, TareasTipo tareaTipo) {
+    public String tareasporproceso(@RequestParam(value = "nombre") String urlParam, Model model, TareasEjecutadas tarea) {
         var nombre = urlParam;
         var procesotipo = procesosTipoDao.findByNombre(nombre);
         var idProceso = procesosTipoService.getId(nombre);
@@ -81,7 +84,8 @@ public class ControladorProceso {
         var nombreCompleto = funcionariosService.nombreCompleto();
         var rolSaludo = funcionariosService.rolSaludo();
         var funcionarios = funcionariosDao.findByIdSubunidad(funcionariosService.idSubunidad());
-        
+        var noimplementado = "no implementado";
+
         model.addAttribute("procesotipo", procesotipo);
         model.addAttribute("nombre", nombre);
         model.addAttribute("idProceso", idProceso);
@@ -89,17 +93,52 @@ public class ControladorProceso {
         model.addAttribute("nusuario", nombreCompleto);
         model.addAttribute("rolsaludo", rolSaludo);
         model.addAttribute("funcionarios", funcionarios);
-        
+        model.addAttribute("noimplementado", noimplementado);
 
         return "ejecutarproceso";
     }
 
-     @GetMapping(value = {"/responsableTarea"})
-    public String responsableTarea(TareasTipo tareasTipo) {
+    @GetMapping(value = {"/ejecuta"})
+    public ModelAndView ejecutar(@RequestParam(value = "idSubunidad") Integer urlParam,
+                            @RequestParam(value = "nombreProceso") String nombreProceso,
+                            @RequestParam(value = "descripcionProceso") String descipcionroceso,
+                            @RequestParam(value = "fechaInicioProceso") String fechaInicioProceso,
+                            @RequestParam(value = "runDisenador") String runDisenador, //hasta aqui son parametros para el proceso ejecutado
+                            @RequestParam(value = "nombreTarea") List<String> nombreTarea, //desde aqui parametros para tarea ejecutada
+                            @RequestParam(value = "descripcionTarea") List<String> descripcionTarea,
+                            @RequestParam(value = "duracionTarea") List<Integer> duracionTarea,
+                            @RequestParam(value = "fechaInicioTarea") List<String> fechaInicioTarea,
+                            @RequestParam(value = "responsableTarea") List<String> responsableTarea,
+                            Model model, 
+                            TareasEjecutadas tarea) {
         
-        
-        return "ejecutarproceso";
+    //RUN DEL USUARIO CONECTADO, para proceso ejecutado y para tarea ejecutada
+    var runUser = funcionariosService.runResponsable();
+    
+    //crear Proceso ejecutado
+    
+    //si lo crea, crear tareas ejecutadas
+    
+    
+    
+    //Colocar ID del proceso, resultante del procedimiento esto deberia ser cuando es correcto
+    var idProcesoEjecutado = 12;
+ 
+        return new ModelAndView("redirect:/ejecuta2?idProcesoEjecutado=" + idProcesoEjecutado);
     }
+   
+    
+    
+    
+    //Ventana para administrar las tareas antes de confirmar
+        @GetMapping(value = {"/ejecuta2"})
+    public ModelAndView ejecutar(@RequestParam(value = "idProcesoEjecutado") Integer urlParam,
+                            Model model, 
+                            TareasEjecutadas tarea) {
+        
 
+ 
+        return new ModelAndView("prueba");
+    }
     
 }
